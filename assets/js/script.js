5c11868598ceeceea2e474597f3ee788893bbee4
Ted J <tedmjablonowski@gmail.com>
2022-02-23 12:55:35 -0500

    Modile issues adjusted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,1 @@
 var riddlerQuestions = ["What hero did Batman's first Robin eventally become?", "Who is Batman's clown-themed adversary?",  "What is Batman's secret identity?"]
-
-
-button.addEventListener('click')
