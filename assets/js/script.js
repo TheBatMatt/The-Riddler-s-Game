f75e751ab0e63b9776969161fa61c9a0bed7eab9
Ted J <tedmjablonowski@gmail.com>
2022-02-23 10:19:22 -0500

    HTML and CSS ready to go!

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var riddlerQuestions = ["What hero did Batman's first Robin eventally become?", "Who is Batman's clown-themed adversary?",  "What is Batman's secret identity?"]
+
+
+button.addEventListener('click')
